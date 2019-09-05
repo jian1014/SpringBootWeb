66cda6ba7fb64161c81eb192ee3a18b030f7841c
jian1014 <1013568167@qq.com>
2019-09-05 17:42:26 +0800

    增加layui框架文件

--- src/main/java/com/xiaojian/springbootweb/controller/LonginController.java
+++ src/main/java/com/xiaojian/springbootweb/controller/LonginController.java
@@ -1,7 +1,6 @@
 package com.xiaojian.springbootweb.controller;
 
 import com.xiaojian.springbootweb.service.LoginService;
-import com.xiaojian.springbootweb.service.impl.LoginImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
